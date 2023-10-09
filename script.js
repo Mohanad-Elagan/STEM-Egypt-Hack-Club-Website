d031702318670e146db27d855c178c26f3484d34
Mohanad-Elagan <81828865+Mohanad-Elagan@users.noreply.github.com>
2023-10-09 18:34:21 +0300

    back

--- script.js
+++ script.js
@@ -13,49 +13,10 @@ var i = 0;
 
 function typeWriter() {
   if (i < quirky.length) {
-    try {
-      document.getElementById("form").innerHTML += quirky.charAt(i);
-    }
-    catch(err) {
-      console.log("NO ID FORM");
-    }
-   
+    document.getElementById("form").innerHTML += quirky.charAt(i);
     i++;
     setTimeout(typeWriter, speed);
   }
 }
 typeWriter();
 
-
-
-// });
-
-window.onresize = function(event) {
-  if(screen.width > 768)
-  {
-    document.getElementsByClassName('nav-items-menu')[0].style.transition = "0s";
-    document.getElementsByClassName('nav-items-menu')[0].style.maxHeight = null;
-  }
-  else 
-  {
-    document.getElementsByClassName('nav-items-menu')[0].style.transition = "max-height 0.2s ease-out";    
-
-  }
-};
-
-
-
-var coll = document.getElementsByClassName("nav-menu-button");
-var i;
-
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = document.getElementsByClassName("nav-items-menu")[0];
-    if (content.style.maxHeight){
-      content.style.maxHeight = null;
-    } else {
-      content.style.maxHeight = content.scrollHeight + "px";
-    }
-  });
-}
