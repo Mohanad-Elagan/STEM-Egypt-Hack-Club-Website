d06f7430f744a33abf51287f5e3615cb4ff56c5f
Karim3tef <92987017+Karim3tef@users.noreply.github.com>
2023-11-12 19:20:50 +0200

    contact.js
    Toggle between new and past boards

--- contact.js
+++ contact.js
@@ -10,3 +10,19 @@
     })
   });
 })();
+
+$(document).ready(function() {
+    $('.year_2022').click(function() {
+      $('.year_2022').addClass('selected');
+      $('.year_2023').removeClass('selected');
+      $('.board-2023').addClass('hidden'); 
+      $('.board-2022').removeClass('hidden');
+    });
+
+    $('.year_2023').click(function() {
+      $('.year_2023').addClass('selected');
+      $('.year_2022').removeClass('selected');
+      $('.board-2022').addClass('hidden'); 
+      $('.board-2023').removeClass('hidden');
+    });
+  });
